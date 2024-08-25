339e4e85f5cfaa1ebfd6558614f8f1c2663335f3
Aakash Bhandary <aakashbhandary3@gmail.com>
2024-08-25 19:36:54 -0400

    file folder structure

---
+++ template.py
@@ -0,0 +1,61 @@
+import os
+from pathlib import Path
+
+file_list = [
+    # for writing workflows and configurations for deployment
+    ".github/workflows/.gitkeep", 
+
+    # source code components
+    "./src/__init__.py",
+    "./src/components/__init__.py",
+    "./src/components/data_ingestion.py",
+    "./src/components/data_transformation.py",
+    "./src/components/model_trainer.py",
+    "./src/components/model_evaluation.py",
+
+    # source code for pipelining the components
+    "./src/pipeline/__init__.py",
+    "./src/pipeline/training_pipeline.py",
+    "./src/pipeline/prediction_pipeline.py",
+    
+    # utility functions
+    "./src/utils/__init__.py",
+    "./src/utils/utils.py",
+
+    # logging and exceptions
+    "./src/logger/logging.py",
+    "./src/exception/exception.py",
+
+    # testing -> (unit and integration testing)
+    "./tests/unit/__init__.py",
+    "./tests/integration/__init__.py",
+
+    # setup script
+    "init_setup.sh",
+
+    # production requirements
+    "requirements.txt",
+
+    # development requirements
+    "requirements_dev.txt",
+
+    # setup files
+    "setup.py",
+    "setup.config",
+    "pyproject.toml",
+    "tox.ini",
+
+    # files for experimenting
+    "./experiments/experiments.ipynb"
+]
+
+for file_path in file_list:
+    file_path = Path(file_path)
+    file_dir, file_name = os.path.split(file_path)
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        # logging.info(f"Creating directory: {file_dir} for file: {file_name}")
+
+    if (not os.path.exists(file_path)) or (os.path.getsize(file_path) == 0):
+        with open(file_path, "w") as f:
+            pass # creates an empty file
